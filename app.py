4b058b59e0a0f8d40a378ffeaa34fdc46e2db78e
margaretalice <margaretwoods1982@gmail.com>
2020-04-28 09:50:52 +0000

    Fix Heroku deployment issues

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-import os 
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello ():
+def hello():
     return "Hello World...again"
 
 
-if __name__== "__main__" :
-    app.run(host= os.environ.get("IF")),
-    port=init(os.environ.get("PORT")),
-    debug=TRUE
-    
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP", "0.0.0.0"),
+            port=int(os.environ.get("PORT", "5000")),
+            debug=True)
