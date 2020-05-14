29951ca5cbd9fed19c4b30d226d59fb22bd8cfd4
margaretalice <margaretwoods1982@gmail.com>
2020-05-14 07:24:20 +0000

    app.py updated

--- app.py
+++ app.py
@@ -11,16 +11,17 @@ app.config["MONGO_URI"] = 'mongodb+srv://Alice:Bedford@myfirstcluster-yzvdc.mong
 mongo = PyMongo(app)
 
 
-@app.route("/")
+@app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
+
 
 @app.route('/add_task')
 def add_task():
-     return render_template('addtask.html',
+    return render_template('addtask.html',
                            categories=mongo.db.categories.find())
-
 
 
 if __name__ == "__main__":
